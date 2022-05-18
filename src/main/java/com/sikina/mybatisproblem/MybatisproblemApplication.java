8c52396bcda3f21c67e73415ff78d5ee259b8f17
Luke Sikina <lucas@ds.dfci.harvard.edu>
2022-05-18 10:16:44 -0400

    Spring Initializr

---
+++ src/main/java/com/sikina/mybatisproblem/MybatisproblemApplication.java
@@ -0,0 +1,13 @@
+package com.sikina.mybatisproblem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisproblemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisproblemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sikina/mybatisproblem/MybatisproblemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sikina.mybatisproblem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisproblemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
